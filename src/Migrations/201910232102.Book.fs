76a4f5c4bfbbdb42fc103c85f5c4a4ab0cb8a4f9
Michel Perez <michel.ingesoft@gmail.com>
2019-10-23 21:27:33 -0500

    wip

---
+++ src/Migrations/201910232102.Book.fs
@@ -0,0 +1,13 @@
+namespace Migrations
+
+open SimpleMigrations
+
+[<Migration(201910232102L, "Create Books")>]
+type CreateBooks() =
+    inherit Migration()
+    override __.Up() = base.Execute(@"CREATE TABLE Books(
+      id TEXT NOT NULL,
+      title TEXT NOT NULL,
+      author TEXT NOT NULL
+    )")
+    override __.Down() = base.Execute(@"DROP TABLE Books")
