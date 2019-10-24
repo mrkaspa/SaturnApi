e400c6c7cb62894086718f90d616d86230abb5db
Michel Perez <michel.ingesoft@gmail.com>
2019-10-23 23:04:53 -0500

    wip

--- build.fsx
+++ build.fsx
@@ -13,42 +13,32 @@ let dotnetcliVersion = DotNet.getSDKVersionFromGlobalJson()
 
 Target.create "Clean" ignore
 
-Target.create "InstallDotNetCore" (fun _ ->
-  DotNet.install
-    (fun o -> { o with Version = DotNet.CliVersion.Version dotnetcliVersion })
-  |> ignore
-)
+Target.create "InstallDotNetCore"
+    (fun _ -> DotNet.install (fun o -> { o with Version = DotNet.CliVersion.Version dotnetcliVersion }) |> ignore)
 
-Target.create "Restore" (fun _ ->
-    DotNet.restore id projectPath
-)
+Target.create "Restore" (fun _ -> DotNet.restore id projectPath)
 
-Target.create "Build" (fun _ ->
-    DotNet.build id projectPath
-)
+Target.create "Build" (fun _ -> DotNet.build id projectPath)
 
 
 Target.create "Run" (fun _ ->
-  let server = async {
-    DotNet.exec (fun p -> { p with WorkingDirectory = appPath } ) "watch" "run" |> ignore
-  }
-  let browser = async {
-    Thread.Sleep 5000
-    Process.start (fun i -> { i with FileName = "http://localhost:8085" }) |> ignore
-  }
+    let server = async { DotNet.exec (fun p -> { p with WorkingDirectory = appPath }) "watch" "run" |> ignore }
 
-  [ server; browser]
-  |> Async.Parallel
-  |> Async.RunSynchronously
-  |> ignore
-)
+    let browser =
+        async {
+            Thread.Sleep 5000
+            Process.start (fun i -> { i with FileName = "http://localhost:8085" }) |> ignore
+        }
+    [ server; browser ]
+    |> Async.Parallel
+    |> Async.RunSynchronously
+    |> ignore)
 
+"Clean" ==> "InstallDotNetCore" ==> "Build"
 "Clean"
-  ==> "InstallDotNetCore"
-  ==> "Build"
-
 "Clean"
-  ==> "Restore"
-  ==> "Run"
+"Clean"
+
+"Clean" ==> "Restore" ==> "Run"
 
 Target.runOrDefault "Build"
